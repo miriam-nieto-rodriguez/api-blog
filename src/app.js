16a356682ab71cec33ec3a395a7cd00fde2fc74a
Miriam Nieto <miriam.nieto726@gmail.com>
2026-06-03 16:54:22 +0200

    feat: crear rutas base para autores y posts

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
-// Creation and configuration of the Express APP
 const express = require("express");
-
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
-// Route configuration
-// Example:
-// const apiRoutes = require('./routes/api.routes');
-// app.use('/api', apiRoutes);
+app.use('/api', require('./routes/api.routes.js'));
 
-// 404 handler
+
 app.use((req, res, next) => {
     res.status(404).json({
         message: "Not found"
     });
 });
 
-// Error handler
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: err.message });
